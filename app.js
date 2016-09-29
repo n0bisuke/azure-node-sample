8b5cb56a96dc411661d4434b0649c2cd7ae2406f
のびすけ <rsksound@gmail.com>
2016-09-29 11:31:19 +0900

    update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 1337;
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 });
  
-app.listen(3000);
+app.listen(PORT);
