7c168e6ab8cdfe8cb94ec6ae56a88d6e50fdbd8f
のびすけ <rsksound@gmail.com>
2016-09-30 15:49:04 +0900

    node.jsでbasic認証

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const basicAuth = require('basic-auth-connect');
 
+app.use(basicAuth('n0bisuke', 'sugawara'));
 app.get('/', (req, res) => {
   res.send('Hello World')
 });
